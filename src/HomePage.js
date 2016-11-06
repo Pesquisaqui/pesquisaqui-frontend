0455424bcc72fb6904e561d4605970ef3a95dc43
Ricardo Lemos <Ricardo Lemos>
2016-11-06 11:36:52 -0200

    Added HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import logo from './logo.png'
 import profilepic from './profilepic.png'
+import search from "./search.png"
+import book from "./book.png"
 
 const HomePage = (props) => (
   <div>
@@ -9,24 +11,24 @@ const HomePage = (props) => (
     </div>
     <div className="row">
       <div className="col-lg-4">
-        <img src={profilepic} className="mx-auto d-block"height="140" width="140"/>
-        <h2>Meu Perfil</h2>
-        <p>
+          <img src={profilepic} className="mx-auto d-block"height="140" width="140"/>+
+        <h2 className="text-xs-center">Meu Perfil</h2>
+        <p className="text-xs-center">
           Acesse o seu perfil
         </p>
       </div>
       <div className="col-lg-4">
-        <img src={profilepic} className="mx-auto d-block"height="140" width="140"/>
-        <h2>Meu Perfil</h2>
-        <p>
-          Acesse o seu perfil
+        <img src={search} className="mx-auto d-block"height="140" width="140"/>
+        <h2 className="text-xs-center">Busca</h2>
+        <p className="text-xs-center">
+          Procure um pesquisador na área desejada
         </p>
       </div>
       <div className="col-lg-4">
-        <img src={profilepic} className="mx-auto d-block"height="140" width="140"/>
-        <h2>Meu Perfil</h2>
-        <p>
-          Acesse o seu perfil
+        <img src={book} className="mx-auto d-block"height="140" width="140"/>
+        <h2 className="text-xs-center">Metodologia</h2>
+        <p className="text-xs-center">
+          Acesse nosso portal de metodologia
         </p>
       </div>
     </div>
